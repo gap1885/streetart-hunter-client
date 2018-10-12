a1f29f1b0072e97a4dc807382edb8c8fe3e8081f
Gabriela Arias <gap1885@gmail.com>
2018-10-12 13:12:22 +0200

    login works

--- src/app/pages/streetart-profile/streetart-profile.component.ts
+++ src/app/pages/streetart-profile/streetart-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-streetart-profile',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StreetartProfileComponent implements OnInit {
 
-  constructor() { }
+  user: any;
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.user = this.authService.getUser();
+
   }
 
 }
